e34f467e4124a6d193bcecb0fbf470936c0ce779
Alon Levy <alon@pobox.com>
2015-10-26 01:30:35 +0200

    server: fix client breakage on document names with quotes in name

--- rhizi/rz_api.py
+++ rhizi/rz_api.py
@@ -78,8 +78,8 @@ def rz_mainpage(rzdoc_name=None, aifnode_name=None):
             return common_resp_handle__client_error(status=404)
     # establish rz_config template values
     host_addr, host_port = request.host_sock_addr[0], request.host_sock_addr[1]
-    rz_config = {'rz_config__aifnode_cur__name': s_aifnode_name,
-                 'rz_config__rzdoc_cur__name': s_rzdoc_name,
+    rz_config = {'rz_config__aifnode_cur__name': s_aifnode_name.replace('"', r'\"'),
+                 'rz_config__rzdoc_cur__name': s_rzdoc_name.replace('"', r'\"'),
                  'rz_config__rzdoc__mainpage_name': rz_config.rzdoc__mainpage_name,
                  'rz_config__hostname': host_addr,
                  'rz_config__port': host_port,
